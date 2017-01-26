863af2cc31c7233bd1aa69f0db32198a22d73608
psato <forhighlow@gmail.com>
2017-01-26 15:13:28 +0900

    add audio recording module

---
+++ AVRecorderSample-master/AVRecorderSample/ViewController.swift
@@ -0,0 +1,71 @@
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    // file操作をするNSFileManager
+    // 録音したファイルをDocmentsディレクトリに保存
+    // 他にもtmpディレクトリやLibrary/Cachesディレクトリなんかが
+    let fileManager = NSFileManager()
+    var audioRecorder: AVAudioRecorder?
+    var audioPlayer: AVAudioPlayer?
+    let fileName = "sample.caf"
+    
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var playButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setupAudioRecorder()
+    }
+
+    // 録音ボタンを押した時の挙動
+    @IBAction func pushRecordButton(sender: AnyObject) {
+        audioRecorder?.record()
+    }
+    
+    // 再生ボタンを押した時の挙動
+    @IBAction func pushPlayButton(sender: AnyObject) {
+        self.play()
+    }
+
+    // 録音するために必要な設定を行う
+    // viewDidLoad時に行う
+    func setupAudioRecorder() {
+        // 再生と録音機能をアクティブにする
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+        try! session.setActive(true)
+        let recordSetting : [String : AnyObject] = [
+            AVEncoderAudioQualityKey : AVAudioQuality.Min.rawValue,
+            AVEncoderBitRateKey : 16,
+            AVNumberOfChannelsKey: 2,
+            AVSampleRateKey: 44100.0
+        ]
+        do {
+            try audioRecorder = AVAudioRecorder(URL: self.documentFilePath(), settings: recordSetting)
+        } catch {
+            print("初期設定でerror出たよ(-_-;)")
+        }
+    }
+    // 再生
+    func play() {
+        do {
+            try audioPlayer = AVAudioPlayer(contentsOfURL: self.documentFilePath())
+        } catch {
+            print("再生時にerror出たよ(´・ω・｀)")
+        }
+        audioPlayer?.play()
+        
+    }
+    // 録音するファイルのパスを取得(録音時、再生時に参照)
+    // swift2からstringByAppendingPathComponentが使えなくなったので少し面倒
+    func documentFilePath()-> NSURL {
+        let urls = fileManager.URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask) as [NSURL]
+        let dirURL = urls[0]
+        return dirURL.URLByAppendingPathComponent(fileName)
+    }
+    
+    
+}
+
---
+++ AVRecorderSample-master/README.md
@@ -0,0 +1,7 @@
+# AVRecorderSample
+
+
+
+http://qiita.com/yonell/items/2abc7ce9d7f598bf6409
+
+ここのサイトを参考にした．

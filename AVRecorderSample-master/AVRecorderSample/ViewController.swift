f57703bbf9638be76c1afb8acdbf82f7fee525c3
e155726 <e155726@ie.u-ryukyu.ac.jp>
2017-02-01 00:56:10 +0900

    fix to iOS10.2

--- AVRecorderSample-master/AVRecorderSample/ViewController.swift
+++ AVRecorderSample-master/AVRecorderSample/ViewController.swift
@@ -8,7 +8,7 @@ class ViewController: UIViewController {
     // 端末のDocumentsディレクトリに保存された情報を再生画面で読み込む.
     // 機能的に独立しているため,一つの案として，カメラと録音のアプリを独立させ，機能を起動させるボタンを押すと他のアプリケーションが起動する，というのはどうだろうか．
     // あるいは，実機のアプリケーション"写真"の中に入っている場合，そこから時間軸からノートの中に埋め込む
-    let fileManager = NSFileManager()
+    let fileManager = FileManager()
     var audioRecorder: AVAudioRecorder?
     var audioPlayer: AVAudioPlayer?
     let fileName = "sample.caf"
@@ -22,12 +22,12 @@ class ViewController: UIViewController {
     }
 
     // 録音ボタンを押した時の挙動
-    @IBAction func pushRecordButton(sender: AnyObject) {
+    @IBAction func pushRecordButton(_ sender: AnyObject) {
         audioRecorder?.record()
     }
     
     // 再生ボタンを押した時の挙動
-    @IBAction func pushPlayButton(sender: AnyObject) {
+    @IBAction func pushPlayButton(_ sender: AnyObject) {
         self.play()
     }
 
@@ -39,13 +39,13 @@ class ViewController: UIViewController {
         try! session.setCategory(AVAudioSessionCategoryPlayAndRecord)
         try! session.setActive(true)
         let recordSetting : [String : AnyObject] = [
-            AVEncoderAudioQualityKey : AVAudioQuality.Min.rawValue,
-            AVEncoderBitRateKey : 16,
-            AVNumberOfChannelsKey: 2,
-            AVSampleRateKey: 44100.0
+            AVEncoderAudioQualityKey : AVAudioQuality.min.rawValue as AnyObject,
+            AVEncoderBitRateKey : 16 as AnyObject,
+            AVNumberOfChannelsKey: 2 as AnyObject,
+            AVSampleRateKey: 44100.0 as AnyObject
         ]
         do {
-            try audioRecorder = AVAudioRecorder(URL: self.documentFilePath(), settings: recordSetting)
+            try audioRecorder = AVAudioRecorder(url: self.documentFilePath(), settings: recordSetting)
         } catch {
             print("初期設定でerror出たよ(-_-;)")
         }
@@ -53,7 +53,7 @@ class ViewController: UIViewController {
     // 再生
     func play() {
         do {
-            try audioPlayer = AVAudioPlayer(contentsOfURL: self.documentFilePath())
+            try audioPlayer = AVAudioPlayer(contentsOf: self.documentFilePath())
         } catch {
             print("再生時にerror出たよ(´・ω・｀)")
         }
@@ -62,10 +62,10 @@ class ViewController: UIViewController {
     }
     // 録音するファイルのパスを取得(録音時、再生時に参照)
     // swift2からstringByAppendingPathComponentが使えなくなったので少し面倒
-    func documentFilePath()-> NSURL {
-        let urls = fileManager.URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask) as [NSURL]
+    func documentFilePath()-> URL {
+        let urls = fileManager.urls(for: .documentDirectory, in: .userDomainMask) as [URL]
         let dirURL = urls[0]
-        return dirURL.URLByAppendingPathComponent(fileName)
+        return dirURL.appendingPathComponent(fileName)
     }
     
     
